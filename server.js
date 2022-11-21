d0102340650255bbb6f2814cbff82339b4e36eb0
Gmendon12 <gmendon46@gmail.com>
2022-11-21 23:48:05 +0530

    technology

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const data1 = require('./Routes/Bollywood')
+const data2 = require('./Routes/Technology')
 const cors = require('cors')
 
 app.use(cors({
@@ -9,6 +10,7 @@ app.use(cors({
 
 
 app.use('/home', data1)
+app.use('/home', data2)
 
 app.get('/', (req,res)=>{
 res.write("hello");
