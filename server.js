0c02a6156a80ea0172c27775e8c9d31df633aed4
Gmendon12 <107698788+Gmendon12@users.noreply.github.com>
2022-11-21 18:13:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express()
 const data1 = require('./Routes/Bollywood')
 const cors = require('cors')
 
-app.use(cors)
+app.use(cors({
+    origin:"*", //most preferred
+}))
+
+
 app.use('/home', data1)
+
+app.get('/', (req,res)=>{
+res.write("hello");
+    res.end()
+})
 
 
 app.listen(process.env.PORT||8080, ()=>{
