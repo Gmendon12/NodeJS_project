68dae2babcee148b991360346cc8b517fa3dc802
Gmendon12 <107698788+Gmendon12@users.noreply.github.com>
2022-11-21 18:04:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const app = express()
 const data1 = require('./Routes/Bollywood')
 const cors = require('cors')
 
-
+app.use(cors)
 app.use('/home', data1)
 
-app.use(cors)
 
 app.listen(process.env.PORT||8080, ()=>{
     console.log("Server is running")
