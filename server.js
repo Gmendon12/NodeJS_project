2fabcf6ca78dfe09e356ba4eebb9b00ff707a054
Gmendon12 <gmendon46@gmail.com>
2022-11-22 13:26:04 +0530

    updated

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const data = require("./Routes/Home")
+const datah2 = require("./Routes/Home1")
+const datah3 = require("./Routes/Home2")
+const datah4 = require("./Routes/Home3")
+
 const data1 = require('./Routes/Bollywood')
 const data2 = require('./Routes/Technology')
 const data3 = require('./Routes/Hollywood')
@@ -11,6 +16,10 @@ app.use(cors({
     origin:"*", //most preferred
 }))
 
+app.use('/home', data)
+app.use('/home', datah2)
+app.use('/home', datah3)
+app.use('/home', datah4)
 
 app.use('/home', data1)
 app.use('/home', data2)
