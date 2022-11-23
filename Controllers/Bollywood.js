5f1ecd7f7c6acddd7b2db8e66f49567572b11b1e
Gmendon12 <gmendon46@gmail.com>
2022-11-23 23:38:26 +0530

    json added

--- Controllers/Bollywood.js
+++ Controllers/Bollywood.js
@@ -2,36 +2,43 @@ const bollywood = (req,res) =>{
     res.send(
         [
             {
+                id : 1,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/mangal.jpg")
             },
             {
+                id : 2,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/Hrithik.webp")
             },
             {
+                id : 3,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/salman.jpg")
             },
             {
+                id : 4,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/akshay.jpg")
             },
             {
+                id : 5,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/Bwimages.jpg")
             },
             {
+                id : 6,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/bollywoodimage.jpg")
             },
             {
+                id : 7,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
                 backgroundB : ("images/bollywoodimage.jpg")
