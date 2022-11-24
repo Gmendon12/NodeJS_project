06e21b97f1d5be5dcdb746c768c7f9f44605dbcb
Gmendon12 <gmendon46@gmail.com>
2022-11-24 15:21:18 +0530

    hi

--- Controllers/Bollywood.js
+++ Controllers/Bollywood.js
@@ -5,43 +5,43 @@ const bollywood = (req,res) =>{
                 id : 1,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/mangal.jpg")
+                backgroundB : ("\images/mangal.jpg")
             },
             {
                 id : 2,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/Hrithik.webp")
+                backgroundB : ("\images/Hrithik.webp")
             },
             {
                 id : 3,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/salman.jpg")
+                backgroundB : ("\images/salman.jpg")
             },
             {
                 id : 4,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/akshay.jpg")
+                backgroundB : ("\images/akshay.jpg")
             },
             {
                 id : 5,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/Bwimages.jpg")
+                backgroundB : ("\images/Bwimages.jpg")
             },
             {
                 id : 6,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/bollywoodimage.jpg")
+                backgroundB : ("\images/bollywoodimage.jpg")
             },
             {
                 id : 7,
                 headerB : "Catch waves with an adventure guide",
                 descriptionB: "Gujarat is vastly underrated and its a mystery to us why the region isnt more well known as a tourist destination.",
-                backgroundB : ("images/bollywoodimage.jpg")
+                backgroundB : ("\images/bollywoodimage.jpg")
             },
         ]
     )
