55f81b9500d2f6fb4739edbaa808737a34313b09
Gmendon12 <gmendon46@gmail.com>
2022-11-24 14:24:32 +0530

    params

--- Routes/Bollywood.js
+++ Routes/Bollywood.js
@@ -3,7 +3,10 @@ const app = express()
 const data = require('../Controllers/Bollywood')
 
 const dataRouter = express.Router()
-dataRouter.route('/bollywood')
-.get(data.bollywooddata)
+dataRouter.route('/bollywood').get(data.bollywooddata)
+dataRouter.get('/bollywood/:id', (req,res) =>{
+    console.log(req.params.id);
+    res.send(data)
+})
 
 module.exports = dataRouter
